449a2266551d0ee225ffabace06085a40f78e352
Nour al-Zaghloul <Nouralzaghloul@gmail.com>
2019-09-12 00:31:06 -0400

    IR pigeon_dottable working!

--- R/pigeon_dottable.R
+++ R/pigeon_dottable.R
@@ -1,36 +1,50 @@
 pigeon_dottable <- function(minRatio = .5, maxRatio = .99, steps = .01,
                             minDots = 12){
 
+  # TODO: Replace magrittr
   # TODO: Choice to supply own set of ratios or quantities
+  # TODO: Choose Controls, currecntly only IR, +Density/CArea/CPeri
   ANSRatio <- seq(minRatio, maxRatio, by = steps)
   fractions <- MASS::as.fractions(ANSRatio)
   fractions <- as.data.frame(stringr::str_split(fractions, "/", simplify = TRUE))
   colnames(fractions) <- c("QuantA", "QuantB")
   fractions$QuantA <- type.convert(fractions$QuantA)
   fractions$QuantB <- type.convert(fractions$QuantB)
-  #TODO: Fix below.
-  fractions <- dplyr::mutate(
-    Multiplier = ceiling(QuantA/minDots),
-    QuantA = QuantA * Multiplier,
-    QuantB = QuantB * Multiplier
-  )
+  fractions$Multiplier <- ceiling(minDots/fractions$QuantA)
+  fractions$QuantA <- fractions$Multiplier * fractions$QuantA
+  fractions$QuantB <- fractions$Multiplier * fractions$QuantB
   dottable <- data.frame(
     Ratio = ANSRatio,
-    QuantA = fractions[,1],
-    QuantB = fractions[,2],
-    # TODO: Everything below this line
-    IRadius = rep(1,length(ANSRatio)),
-    CArea = rep(1,length(ANSRatio)),
-    CPerimeter = rep(1,length(ANSRatio)),
-    LocationSize = rep(20,length(ANSRatio)),
-    Density_DotXPx = rep(.001, length(ANSRatio)),
-    Density_PxXArea = rep(.001, length(ANSRatio)),
-    Control = rep(NA, times =  length(ANSRatio)),
-    Serial = seq(length(ANSRatio))
+    QuantA = fractions$QuantA,
+    QuantB = fractions$QuantB
+  ) %>% dplyr::mutate(
+    LocationSize = 20,
+    IRadius = runif(length(ANSRatio), min = .01, max = LocationSize/(2*QuantB)),
+    CAreaA = QuantA * IRadius^2 * pi,
+    CAreaB = QuantB * IRadius^2 * pi,
+    CPeriA = QuantA * 2 * pi * IRadius,
+    CPeriB = QuantB * 2 * pi * IRadius,
+    Control = "IR"
+    #TODO: Add Density_px/Density_quant
+    #TODO: Add stim ID
   )
-  dottable$StimID <- paste0(dottable$Serial, dottable$Control)
-  return(dottable)
 
+  # dottable <- data.frame(
+  #   Ratio = ANSRatio,
+  #   QuantA = fractions[,1],
+  #   QuantB = fractions[,2],
+  #   # TODO: Everything below this line
+  #   IRadius = runif(1, min = .01, max = 20/(2*fractions$QuantB)),
+  #   CArea = rep(1,length(ANSRatio)),
+  #   CPerimeter = rep(1,length(ANSRatio)),
+  #   LocationSize = rep(20,length(ANSRatio)),
+  #   Density_DotXPx = rep(.001, length(ANSRatio)),
+  #   Density_PxXArea = rep(.001, length(ANSRatio)),
+  #   Control = rep(NA, times =  length(ANSRatio)),
+  # #   Serial = seq(length(ANSRatio))
+  # )
+  # dottable$StimID <- paste0(dottable$Serial, dottable$Control)
+  dottable
 }
 
 #### Goals ----

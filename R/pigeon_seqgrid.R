51c9bee8922ea7fc57c267d9fdd519967fd63e5a
Nour al-Zaghloul <Nouralzaghloul@gmail.com>
2019-10-18 10:44:16 -0400

    Update pigeon_seqgrid.R

--- R/pigeon_seqgrid.R
+++ R/pigeon_seqgrid.R
@@ -1,4 +1,24 @@
-pigeon_seqgrid <- function(x, ...){
+pigeon_seqgrid <- function(from, to, by){
+  # This is just a simple way to create a grid sequences using
+  #   seq() and expand.grid() for all the combinations possible
 
+
+  # error catching:
+  #   From & To Lengths don't match or they are not 1L
+  #   From, To, By are missing arguments
+  #   by causes From -> To to go in the wrong direction
+
+  # Create Empty Dataframe
+  seqgrid <- data.frame()
+
+  # Run loop to
+  # TODO: Change to sapply eventually
+  for(i in seq(length(from))){
+  }
+
+  # expand.grid function
+
+
+  # returns the answer
 
 }

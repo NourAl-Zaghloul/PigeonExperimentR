962c50eaf543a0ec750a5e1d474195390d07e45b
NourAl-Zaghloul <nzaghlou@andrew.cmu.edu>
2019-10-29 12:41:10 -0400

    pigeon_seqGrid is up & running!

---
+++ R/pigeon_genCounterbalance.R
@@ -0,0 +1,5 @@
+# To generate orders/trial structure
+pigeon_genCounterbalance <- function(..., type){
+  # type to include superpermutations for within subject interleaved
+
+}
--- R/pigeon_seqgrid.R
+++ R/pigeon_seqgrid.R
@@ -1,24 +1,35 @@
 pigeon_seqgrid <- function(from, to, by){
   # This is just a simple way to create a grid sequences using
   #   seq() and expand.grid() for all the combinations possible
+  #   TODO: optional name parameter as well (must match length of from)
+  #     otherwise: x,y,z if length(from) <= 3, or
 
-
-  # error catching:
+  # TODO: error catching
+  ##### error catching: ----
   #   From & To Lengths don't match or they are not 1L
+  #     error, fix wrong one
+  #     warning, values not same length as from are set to repeat until length.out=length(from)
   #   From, To, By are missing arguments
+  #     error, create the missing values
   #   by causes From -> To to go in the wrong direction
+  #     warning, reversed the direction automatically; may be wrong if not intention
+  #   grid is too large!
+  #     error, too many values to calculate effectively
+  #   Doesn't have purr installed
 
-  # Create Empty Dataframe
-  seqgrid <- data.frame()
+  ##### Create the full vectors for our variables ----
+  from2 = from
+  to2 = rep(to, length.out = length(from))
+  by2 = rep(by, length.out = length(from))
 
-  # Run loop to
-  # TODO: Change to sapply eventually
-  for(i in seq(length(from))){
-  }
+  #### Creates the dataframe ----
+  # TODO: maybe change to apply & remove dependency
+  data_list <- purrr::pmap(list(from2,to2,by2), seq)
 
-  # expand.grid function
+  ##### expand.grid function----
+  grid_expanded <- expand.grid(data_list)
 
-
-  # returns the answer
+  ##### returns the answer ----
+  return(grid_expanded)
 
 }

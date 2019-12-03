c228769722cc3ad52c6f330fd2aef9bceedf43c2
NourAl-Zaghloul <nzaghlou@andrew.cmu.edu>
2019-12-03 12:04:00 -0500

    Added warning/errors

--- R/pigeon_seqgrid.R
+++ R/pigeon_seqgrid.R
@@ -1,22 +1,34 @@
 pigeon_seqgrid <- function(from, to, by, names.col = NULL){
   # This is just a simple way to create a grid sequences using
   #   seq() and expand.grid() for all the combinations possible
-
-  # TODO: error catching
+  #
+  # TODO: make it so just the longest vector is the reference vector for length, not from every time
+  #
+  #
   ##### error catching: ----
-  #   From & To Lengths don't match or they are not 1L
-  #     error, fix wrong one
-  #     warning, values not same length as from are set to repeat until length.out=length(from)
-  #   From, To, By are missing arguments
-  #     error, create the missing values
-  #   by causes From -> To to go in the wrong direction
-  #     warning, reversed the direction automatically; may be wrong if not intention
-  #   grid is too large!
-  #     error, too many values to calculate effectively
-  #   Doesn't have purr installed
+  #   TODO: grid is too large!
+  #             error, too many values to calculate effectively
+  #   Doesn't have purr installed?
+  if(any(from > to) && any(by[from > to] > 0)){
+    stop("wrong sign in 'by' argument", call. = FALSE)
+  }
+  if(any(by == 0)){
+    stop("'by' cannot equal 0", call. = FALSE)
+  }
 
   ##### Create the full vectors for our variables ----
-  # TODO: Really need a warning created here
+  # TODO: Update warning for when from is not necessarily the longest variable
+  # TODO:     Add 'from' warnings for length
+  if(length(to) > 1 && length(to) != length(from)){
+    warning(paste("'to' variable is > 1, but < length(from),",
+                  "'to' variable set to repeat out to length(from)", sep = "\n"),
+            call. = FALSE)
+  }
+  if(length(by) > 1 && length(by) != length(from)){
+    warning(paste("'by' variable is > 1, but < length(from),",
+                  "'by' variable set to repeat out to length(from)", sep = "\n"),
+            call. = FALSE)
+  }
   from2 = from
   to2 = rep(to, length.out = length(from))
   by2 = rep(by, length.out = length(from))
@@ -29,7 +41,6 @@ pigeon_seqgrid <- function(from, to, by, names.col = NULL){
   grid_expanded <- expand.grid(data_list)
 
   #### variable naming ----
-  # TODO: Warning
   if(exists("names.col") && length(names.col) >= length(from2)){
     names(grid_expanded) <- names.col
   } else if(length(from2) <= 3){
